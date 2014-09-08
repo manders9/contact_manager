946b4214de47449bf9848bb4bbce3966ec67a683
Melissa Anderson <Melissa.Anderson.2014@trincoll.edu>
2014-09-08 16:09:02 -0400

    show.erb created.

--- models/contact.rb
+++ models/contact.rb
@@ -1,11 +1,11 @@
-class Contact
-  attr_reader :first_name, :last_name, :phone_number
+class Contact < ActiveRecord::Base
+  # attr_reader :first_name, :last_name, :phone_number
 
-  def initialize(attributes)
-    @first_name = attributes[:first_name]
-    @last_name = attributes[:last_name]
-    @phone_number = attributes[:phone_number]
-  end
+  # def initialize(attributes)
+  #   @first_name = attributes[:first_name]
+  #   @last_name = attributes[:last_name]
+  #   @phone_number = attributes[:phone_number]
+  # end
 
   def name
     [first_name, last_name].join(' ')
